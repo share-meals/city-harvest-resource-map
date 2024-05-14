1137d3dabb131eed4b072b5cac7fcee0938c7133
Jon Chin <jon@sharebyteaching.org>
2024-05-14 15:03:09 -0400

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,52 @@
+const logFunctionUrl = 'https://us-east1-city-harvest-423311.cloudfunctions.net';
+
 import {
-    useEffect,
-    useRef,
-    useState
+  useEffect,
+  useRef,
+  useState
 } from 'react';
 import {RControl} from 'rlayers';
 
 import {
-    IonApp,
-    IonButton,
-    IonButtons,
-    IonCol,
-    IonContent,
-    IonGrid,
-    IonHeader,
-    IonIcon,
-    IonModal,
-    IonPage,
-    IonRow,
-    IonTitle,
-    IonToolbar,
-    setupIonicReact
+  IonApp,
+  IonButton,
+  IonButtons,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonHeader,
+  IonIcon,
+  IonModal,
+  IonPage,
+  IonRow,
+  IonTitle,
+  IonToolbar,
+  setupIonicReact
 } from '@ionic/react';
 import {
-    GeocoderInput,
-    GeocoderProvider,
-    Map,
-    MapControl,
-    MapProvider,
-    LayerToggles,
-    ZoomButtons,
+  GeocoderInput,
+  GeocoderProvider,
+  Map,
+  MapProvider,
+  LayerToggles,
+  useMap,
 } from '@share-meals/frg-ui';
 
+import {ZoomButtons} from './ZoomButtons';
+
 import type {
-    MapLayer,
-    onGeocode
+  MapLayer,
+  onGeocode
 } from '@share-meals/frg-ui';
 import {Renderer} from './data/Renderer';
 import {useWindowSize} from '@uidotdev/usehooks';
 import {
-    closeSharp,
-    homeSharp,
-    layersSharp
+  closeSharp,
+  homeSharp,
+  layersSharp
 } from 'ionicons/icons';
 
 import './App.scss';
-
-
 
 import food_pantries from './data/food_pantries.json';
 import soup_kitchens from './data/soup_kitchens.json';
@@ -74,211 +75,238 @@ import '@ionic/react/css/display.css';
 import './theme/variables.css';
 
 const layers: MapLayer[] = [
-    {
-	name: 'Community Partner Distributions',
-	geojson: cpds,
-	fillColor: '#D25B73',
-      strokeColor: 'white',
-      icon: {
-	src: cpd_truck,
-	scale: 0.13312
-	}
+  {
+    name: 'Community Partner Distributions',
+    geojson: cpds,
+    fillColor: '#D25B73',
+    strokeColor: 'white',
+    icon: {
+      src: cpd_truck,
+      scale: 0.13312
     },
-    {
-	name: 'Mobile Markets',
-	geojson: mms,
-	fillColor: '#006747',
-	strokeColor: 'white',
-	icon: {
-	    src: mm_truck,
-	    scale: 0.065
-	}
+    type: 'vector'
+  },
+  {
+    name: 'Mobile Markets',
+    geojson: mms,
+    fillColor: '#006747',
+    strokeColor: 'white',
+    icon: {
+      src: mm_truck,
+      scale: 0.065
     },
-    {
-	name: 'Food Pantries',
-	geojson: food_pantries,
-	fillColor: '#64a70b',
-	strokeColor: 'white'
-    },
-    {
-	name: 'Soup Kitchens',
-	geojson: soup_kitchens,
-	fillColor: '#893B67',
-	strokeColor: 'white'
-    }
+    type: 'vector'
+  },
+  {
+    name: 'Food Pantries',
+    geojson: food_pantries,
+    fillColor: '#64a70b',
+    strokeColor: 'white',
+    type: 'vector'
+  },
+  {
+    name: 'Soup Kitchens',
+    geojson: soup_kitchens,
+    fillColor: '#893B67',
+    strokeColor: 'white',
+    type: 'vector'
+  }
 ];
 
 setupIonicReact();
 
 const InfoModal = ({trigger}: {trigger: string}) => {
-    const [isOpen, setIsOpen] = useState<boolean>(false);
-    useEffect(() => {
-	setIsOpen(trigger !== '');
-    }, [trigger]);
-    return <IonModal isOpen={isOpen}>
-	<IonHeader>
-	    <IonToolbar>
-		<IonButtons slot='end'>
-		    <IonButton onClick={() => {setIsOpen(false);}}>
-			<IonIcon slot='icon-only' icon={closeSharp} />
-		    </IonButton>
-		</IonButtons>
-	    </IonToolbar>
-	</IonHeader>
-	<IonContent className='ion-padding'>
-	    <Renderer />
-	</IonContent>
-    </IonModal>;    
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  useEffect(() => {
+    setIsOpen(trigger !== '');
+  }, [trigger]);
+  return <IonModal isOpen={isOpen}>
+    <IonHeader>
+      <IonToolbar>
+	<IonButtons slot='end'>
+	  <IonButton onClick={() => {setIsOpen(false);}}>
+	    <IonIcon slot='icon-only' icon={closeSharp} />
+	  </IonButton>
+	</IonButtons>
+      </IonToolbar>
+    </IonHeader>
+    <IonContent className='ion-padding'>
+      <Renderer />
+    </IonContent>
+  </IonModal>; 
 }
 
 const LayerTogglesModal = () => {
-    const modal = useRef<HTMLIonModalElement>(null);
-    return <IonModal ref={modal} trigger='openLayerTogglesModal'>
-	<IonHeader>
-	    <IonToolbar>
-		<IonButtons slot='end'>
-		    <IonButton onClick={() => {modal.current?.dismiss();}}>
-			<IonIcon slot='icon-only' icon={closeSharp} />
-		    </IonButton>
-		</IonButtons>
-	    </IonToolbar>
-	</IonHeader>
-	<IonContent className='ion-padding'>
-	    <LayerToggles />
-	</IonContent>
-    </IonModal>;
+  const modal = useRef<HTMLIonModalElement>(null);
+  return <IonModal ref={modal} trigger='openLayerTogglesModal'>
+    <IonHeader>
+      <IonToolbar>
+	<IonButtons slot='end'>
+	  <IonButton onClick={() => {modal.current?.dismiss();}}>
+	    <IonIcon slot='icon-only' icon={closeSharp} />
+	  </IonButton>
+	</IonButtons>
+      </IonToolbar>
+    </IonHeader>
+    <IonContent className='ion-padding'>
+      <LayerToggles />
+    </IonContent>
+  </IonModal>;
 }
 
 const logGeocode = ({latlng, address}: onGeocode) => {
-    /*
-       if(onMapCenter !== undefined){
-       onMapCenter({
-       address,
-       lat: latlng?.lat || null,
-       lng: latlng?.lng || null,
-       });
-       }
-       if(latlng !== null){
-       const point: Coordinate = fromLonLat([latlng.lng, latlng.lat]);
-       setView({
-       center: point,
-       zoom: view.zoom
-       });
-       setSpotlight!(new Point(point));
-       setGeocoderErrorMessage(null);
-       }else{
-       setGeocoderErrorMessage('Address not found');
-       }
-     */
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({address, ...latlng})
+  };
+  fetch(`${logFunctionUrl}/log-geocode`, options)
+    .then((response) => {
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  /*
+     if(onMapCenter !== undefined){
+     onMapCenter({
+     address,
+     lat: latlng?.lat || null,
+     lng: latlng?.lng || null,
+     });
+     }
+     if(latlng !== null){
+     const point: Coordinate = fromLonLat([latlng.lng, latlng.lat]);
+     setView({
+     center: point,
+     zoom: view.zoom
+     });
+     setSpotlight!(new Point(point));
+     setGeocoderErrorMessage(null);
+     }else{
+     setGeocoderErrorMessage('Address not found');
+     }
+   */
 };
 
 
 const GeocoderModal = () => {
-    const modal = useRef<HTMLIonModalElement>(null);
-    return <IonModal ref={modal} trigger='openGeocoderModal'>
-	<IonHeader>
-	    <IonToolbar>
-		<IonTitle>
-		    Go to an address
-		</IonTitle>
-		<IonButtons slot='end'>
-		    <IonButton onClick={() => {modal.current?.dismiss();}}>
-			<IonIcon slot='icon-only' icon={closeSharp} />
-		    </IonButton>
-		</IonButtons>
-	    </IonToolbar>
-	</IonHeader>
-	<IonContent className='ion-padding'>
-	    <GeocoderInput
-	    onGeocode={(args) => {
-		modal.current?.dismiss();
-		logGeocode(args);
-	    }}
-	    helperText='To find food near you, please enter your address, city, and zip code'
-	    />
-	</IonContent>
-    </IonModal>;
+  const modal = useRef<HTMLIonModalElement>(null);
+  return <IonModal ref={modal} trigger='openGeocoderModal'>
+    <IonHeader>
+      <IonToolbar>
+	<IonTitle>
+	  Go to an address
+	</IonTitle>
+	<IonButtons slot='end'>
+	  <IonButton onClick={() => {modal.current?.dismiss();}}>
+	    <IonIcon slot='icon-only' icon={closeSharp} />
+	  </IonButton>
+	</IonButtons>
+      </IonToolbar>
+    </IonHeader>
+    <IonContent className='ion-padding'>
+      <GeocoderInput
+	onGeocode={(args) => {
+	  modal.current?.dismiss();
+	  logGeocode(args);
+	}}
+	helperText='To find food near you, please enter your address, city, and zip code'
+      />
+    </IonContent>
+  </IonModal>;
 }
 
 export const App = () => {
-    const size: {
-	height: number | null,
-	width: number | null
-    } = useWindowSize();
-    const isMobile: boolean = size.width! < 576;
-    const controls: MapControl[] = [
-	{
-	    className:'primaryButtons',
-	    // @ts-ignore
-	    element: <ZoomButtons />
-	},
-	{
-	    className: 'secondaryButtons',
-	    element: <>
-		<IonButton id='openLayerTogglesModal'>
-		    <IonIcon slot='icon-only' icon={layersSharp} />
-		</IonButton>
-		<IonButton id='openGeocoderModal'>
-		    <IonIcon slot='icon-only' icon={homeSharp} />
-		</IonButton>
-	    </>
-	}
-    ];
-    const [infoTrigger, setInfoTrigger] = useState<string>('');
-    useEffect(() => {
-	if(!isMobile && infoTrigger !== ''){
-	    setInfoTrigger('');
-	}
-    }, [isMobile, infoTrigger, setInfoTrigger]);
-    return <IonApp>
-	<IonPage>
-	    <IonContent>
-		<div style={{height: '100vh', width: '100vw'}}>
-		    <MapProvider
-			center={{
-			    lat: 40.7127281,
-			    lng: -74.0060152
-			}}
-			layers={layers}
-			maxZoom={20}
-			minZoom={10}>
-			<GeocoderProvider
-			    platform='nominatim'
-			    url='https://nominatim.openstreetmap.org/search'>
-			    {!isMobile &&
-			     <IonGrid>
-				 <IonRow style={{height: '100vh'}}>
-				     <IonCol>
-					 <Map
-					     controls={controls.slice(0, 1)}
-					 />
-				     </IonCol>
-				     <IonCol>
-					 <LayerToggles />
-					 <GeocoderInput
-					     onGeocode={logGeocode}
-					     onGeocodeZoom={17}
-					     helperText='To find food near you, please enter your address, city, and zip code'
-					 />
-					 <Renderer />
-				     </IonCol>
-				 </IonRow>
-			     </IonGrid>
-			    }
-			    {isMobile && <>
-				<Map
-				onMapClick={() => {
-				    setInfoTrigger((new Date()).toString());
-				}}
-				controls={controls} />
-				<InfoModal trigger={infoTrigger} />
-				<GeocoderModal />
-				<LayerTogglesModal />
-			    </>}
-			</GeocoderProvider>
-		    </MapProvider>
-		</div>
-	    </IonContent>
-	</IonPage>
-    </IonApp>;
+  const size: {
+    height: number | null,
+    width: number | null
+  } = useWindowSize();
+  const isMobile: boolean = size.width! < 576;
+  const controls: any = [
+    <span className='primaryButtons'>
+      <ZoomButtons />
+    </span>,
+    <span className='secondaryButtons'>
+      <IonButton id='openLayerTogglesModal'>
+	<IonIcon slot='icon-only' icon={layersSharp} />
+      </IonButton>
+      <IonButton id='openGeocoderModal'>
+	<IonIcon slot='icon-only' icon={homeSharp} />
+      </IonButton>
+    </span>
+  ];
+  const onFeatureClick = ({data, lat, lng}: {data: any, lat: number, lng: number}) => {
+    const options = {
+      method: 'POST',
+      headers: {
+	'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({id: data.id, lat, lng})
+    };
+    fetch(`${logFunctionUrl}/log-feature-click`, options)
+      .then((response) => {
+      })
+      .catch((error) => {
+	console.log(error);
+      });
+  };
+  const [infoTrigger, setInfoTrigger] = useState<string>('');
+  useEffect(() => {
+    if(!isMobile && infoTrigger !== ''){
+      setInfoTrigger('');
+    }
+  }, [isMobile, infoTrigger, setInfoTrigger]);
+  return <IonApp>
+    <IonPage>
+      <IonContent>
+	<div style={{height: '100vh', width: '100vw'}}>
+	  <MapProvider
+	    center={{
+	      lat: 40.7127281,
+	      lng: -74.0060152
+	    }}
+	    layers={layers}
+	    maxZoom={20}
+	    minZoom={10}>
+	    <GeocoderProvider
+	      platform='nominatim'
+	      url='https://nominatim.openstreetmap.org/search'>
+	      {!isMobile &&
+	       <IonGrid>
+		 <IonRow style={{height: '100vh'}}>
+		   <IonCol>
+		     <Map
+		       controls={controls.slice(0, 1)}
+		       onFeatureClick={onFeatureClick}
+		     />
+		   </IonCol>
+		   <IonCol>
+		     <LayerToggles />
+		     <GeocoderInput
+		       onGeocode={logGeocode}
+		       onGeocodeZoom={17}
+		       helperText='To find food near you, please enter your address, city, and zip code'
+		     />
+		     <Renderer />
+		   </IonCol>
+		 </IonRow>
+	       </IonGrid>
+	      }
+	      {isMobile && <>
+		<Map
+		  controls={controls}
+		  onFeatureClick={onFeatureClick}
+		/>
+		<InfoModal trigger={infoTrigger} />
+		<GeocoderModal />
+		<LayerTogglesModal />
+	      </>}
+	    </GeocoderProvider>
+	  </MapProvider>
+	</div>
+      </IonContent>
+    </IonPage>
+  </IonApp>;
 }
